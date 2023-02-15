8ced1e88e194e40fc2ba9b22c61cc4a980893fb4
Tim Lau <timlautk@gmail.com>
2023-02-14 20:11:49 -0600

    Update lmc_jax.py

--- lmc_jax.py
+++ lmc_jax.py
@@ -5,17 +5,32 @@ import scipy as sp
 import matplotlib.pyplot as plt
 from scipy.linalg import sqrtm
 
+import ProxNest.utils as utils
+import ProxNest.sampling as sampling
+import ProxNest.optimisations as optimisations
+import ProxNest.operators as operators
+
 import jax
 from jax import grad, jit
 import jax.numpy as jnp
 import jax.scipy as jsp
 
+import aesara
+import aemcmc
+import aehmc
+
 import fire
 
 
 class Langevin:
-    def __init__(self, gamma):
+    def __init__(self, gamma) -> None:
         self.gamma = gamma
+
+
+class HMC:
+  def __init__(self, gamma) -> None:
+     self.gamma = gamma
+
 
 
 if __name__ == '__main__':
