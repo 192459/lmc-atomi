996e549d231a73db204a9ee0739cbd286af025e0
Tim Lau <timlautk@gmail.com>
2023-02-15 21:15:42 -0600

    Update lmc_jax.py

--- lmc_jax.py
+++ lmc_jax.py
@@ -22,6 +22,11 @@ from jax import grad, jit
 import jax.numpy as jnp
 import jax.scipy as jsp
 
+import blackjax
+import optax
+from blackjax.types import PyTree
+from optax._src.base import OptState
+
 # import aesara
 # import aemcmc
 # import aehmc
@@ -62,30 +67,79 @@ class GaussianMixtureSampling:
     plt.title("Samples from a mixture of 25 normal distributions")
 
     plt.show(block=False)
-    plt.pause(3)
+    plt.pause(5)
     plt.close()
-    
+
+
+class SGLD:
+  def __init__(self, lamda, positions, sigma) -> None:
+    self.lamda = lamda 
+    self.positions = positions
+    self.mu = jnp.array([list(prod) for prod in itertools.product(positions, positions)])
+    self.sigma = sigma * jnp.eye(2)
+
+  def logprob_fn(self, x, *_):
+    return self.lamda * jsp.special.logsumexp(jax.scipy.stats.multivariate_normal.logpdf(x, self.mu, self.sigma))
+
+  def sampling(self, seed, num_training_steps):
+    schedule_fn = lambda k: 0.05 * k ** (-0.55)
+    schedule = [schedule_fn(i) for i in range(1, num_training_steps+1)]
+
+    grad_fn = lambda x, _: jax.grad(self.logprob_fn)(x)
+    sgld = blackjax.sgld(grad_fn)
+
+    rng_key = jax.random.PRNGKey(seed)
+    init_position = -10 + 20 * jax.random.uniform(rng_key, shape=(2,))
+
+    position = init_position
+    sgld_samples = []
+    for i in progress_bar(range(num_training_steps)):
+      _, rng_key = jax.random.split(rng_key)
+      position = jax.jit(sgld)(rng_key, position, 0, schedule[i])
+      sgld_samples.append(position)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    x = [sample[0] for sample in sgld_samples]
+    y = [sample[1] for sample in sgld_samples]
+
+    ax.plot(x, y, 'k-', lw=0.1, alpha=0.5)
+    ax.set_xlim([-8, 8])
+    ax.set_ylim([-8, 8])
+
+    plt.axis('off')
+    plt.show(block=False)
+    plt.pause(5)
+    plt.close()
+
+
+class cyclicalSGLD:
+  def __init__(self, gamma) -> None:
+    self.gamma = gamma
+
+
+  def sampling(self, seed, num_training_steps):
+    pass
+
 
 class Langevin:
   def __init__(self, gamma) -> None:
     self.gamma = gamma
 
 
-  def unadjustedLangevin(self):
-    gamma = self.gamma
-
+  def sampling(self):
+    
     return 
 
-
-class cyclicalSGLD:
-    def __init__(self, gamma) -> None:
-      self.gamma = gamma
 
 
 class HMC:
   def __init__(self, gamma) -> None:
     self.gamma = gamma
 
+  def sampling(self):
+
+    pass 
 
 
 if __name__ == '__main__':
@@ -96,5 +150,13 @@ if __name__ == '__main__':
   xmax, ymax = 5, 5
   nbins = 300j
   GaussianMixtureSampling(lamda, positions, sigma).sampling(0, xmin, ymin, xmax, ymax, nbins)
+
+  SGLD().sampling(3, 50000)
+
+  # cyclicalSGLD().sampling()
+
+  # Langevin().sampling()
+
+  # HMC().sampling()
 
   # fire.Fire(Langevin())
