77d0f3027c0127ac6df9b7eead51f06665336fbd
Tim Lau <timlautk@gmail.com>
2023-02-15 20:25:31 -0600

    Update lmc_jax.py

--- lmc_jax.py
+++ lmc_jax.py
@@ -1,8 +1,14 @@
 import os
+import itertools
+from fastprogress import progress_bar
+from typing import NamedTuple
+import fire
+
 import pandas as pd
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
+from scipy.stats import gaussian_kde
 from scipy.linalg import sqrtm
 
 import ProxNest
@@ -20,10 +26,22 @@ import aesara
 import aemcmc
 import aehmc
 
-from fastprogress import progress_bar
 
-import fire
+class generateGaussianMixture:
+  def __init__(self, lamda, positions, sigma) -> None:
+    self.lamda = lamda 
+    self.positions = positions
+    self.mu = jnp.array([list(prod) for prod in itertools.product(positions, positions)])
+    self.sigma = sigma * jnp.eye(2)
 
+  def logprob_fn(self, x, *_):
+    return self.lamda * jsp.special.logsumexp(jax.scipy.stats.multivariate_normal.logpdf(x, self.mu, self.sigma))
+
+  def sample_fn(self, rng_key):
+    choose_key, sample_key = jax.random.split(rng_key)
+    samples = jax.random.multivariate_normal(sample_key, self.mu, self.sigma)
+    return jax.random.choice(choose_key, samples)
+    
 
 class Langevin:
   def __init__(self, gamma) -> None:
@@ -48,4 +66,7 @@ class HMC:
 
 
 if __name__ == '__main__':
+  lmbda = 1/25
+  positions = [-4, -2, 0, 2, 4]
+
   fire.Fire(Langevin())
