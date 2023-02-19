f23fe35b2c396e17d25aabf49037b9fcc3acf424
Tim Lau <timlautk@gmail.com>
2023-02-19 00:38:17 -0600

    Update lmc_jax.py

--- lmc_jax.py
+++ lmc_jax.py
@@ -1,12 +1,29 @@
+# Copyright 2023 by Tim Tsz-Kit Lau
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Install libraries: pip install -U numpy matplotlib scipy seaborn fire ProxNest jax blackjax optax
+
+# Usage: python lmc_jax.py --gamma_ula=7.5e-2 --gamma_mala=7.5e-2 
+# --gamma_pula=8e-2 --gamma_ihpula=5e-4 --gamma_mla=5e-2 --K=5000 --n=5
+
 import os
 import itertools
 from fastprogress import progress_bar
 from typing import NamedTuple
 import fire
 
-import pandas as pd
 import numpy as np
-import scipy as sp
 import matplotlib.pyplot as plt
 from scipy.stats import gaussian_kde
 from scipy.linalg import sqrtm
