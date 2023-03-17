906db8730b91b384747747ba1c1ce5751c3281e7
Tim Lau <timlautk@gmail.com>
2023-03-17 10:25:32 -0500

    Edits

--- expt.sh
+++ expt.sh
@@ -5,3 +5,5 @@ python prox_lmc.py --gamma_pgld=5e-3 --gamma_myula=5e-3 --gamma_mymala=5e-3 --ga
 
 CUDA_VISIBLE_DEVICES=0 python sgld.py --n=50000 --num_partitions=10000 --lr=1e-4
 
+python lmc_laplace.py --gamma_ula=1.2e-1 --gamma_mala=1.2e-1 --gamma_pula=1.2e-1 --gamma_mla=1.2e-1 --lamda=1e0 --alpha=5e-1 --n=3 --K=50000 --seed=0
+
