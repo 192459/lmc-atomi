8a3ccc7093542dbd8ff5b7a1ea4e7159f6f0c6e8
Tim Lau <timlautk@gmail.com>
2023-10-19 03:55:55 -0500

    Update lmc.py

--- lmc.py
+++ lmc.py
@@ -394,7 +394,7 @@ def lmc_gaussian_mixture(gamma_ula=5e-2, gamma_mala=5e-2,
     interval = 100
 
     for k in progress_bar(range(1, K)):
-        if (k-1) % interval == 0 or k == K-1:
+        if (k-1) % interval == 0:
             b = np.ones((k+1,)) / (k+1)
             M_ula = ot.dist(Z1, Z2[:k+1,:])
             M_pula = ot.dist(Z1, Z4[:k+1,:])
@@ -408,13 +408,10 @@ def lmc_gaussian_mixture(gamma_ula=5e-2, gamma_mala=5e-2,
             wass_pula_list.append(wass_pula**.5)
             wass_ihpula_list.append(wass_ihpula**.5)
             wass_mla_list.append(wass_mla**.5)
-    
-    for k in progress_bar(range(1, len(Z3))) :
-        if (k-1) % interval == 0 or k == len(Z3)-1:
-            b = np.ones((k+1,)) / (k+1)
-            M_mala = ot.dist(Z1, Z3[:k+1,:])
-            wass_mala = ot.emd2(a, b, M_mala, numItermax=nitermax, numThreads=16)
-            wass_mala_list.append(wass_mala**.5)
+            if k < len(Z3):
+                M_mala = ot.dist(Z1, Z3[:k+1,:])
+                wass_mala = ot.emd2(a, b, M_mala, numItermax=nitermax, numThreads=16)
+                wass_mala_list.append(wass_mala**.5)
 
     ## Plot of 2-Wasserstein distances vs samples
     mpl.rcParams.update(mpl.rcParamsDefault)
@@ -428,8 +425,8 @@ def lmc_gaussian_mixture(gamma_ula=5e-2, gamma_mala=5e-2,
     )
 
     fig3 = plt.figure(figsize=(6, 4))
-    iters = [k+1 for k in range(1, K) if (k-1) % interval == 0 or k == K-1]
-    iters_mala = [k+1 for k in range(1, len(Z3)) if (k-1) % interval == 0 or k == len(Z3)-1]
+    iters = [k+1 for k in range(1, K) if (k-1) % interval == 0]
+    iters_mala = [k+1 for k in range(1, len(Z3)) if (k-1) % interval == 0]
     plt.plot(iters, wass_ula_list, label='ULA')
     plt.plot(iters_mala, wass_mala_list, label='MALA')
     plt.plot(iters, wass_pula_list, label='PULA')
