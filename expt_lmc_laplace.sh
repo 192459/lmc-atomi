c1edd9c1f96ef35e6cbdf41606bb6460aa434227
Tim Lau <timlautk@gmail.com>
2023-10-22 07:40:08 -0500

    Update expt_lmc_laplace.sh

--- expt_lmc_laplace.sh
+++ expt_lmc_laplace.sh
@@ -4,7 +4,7 @@ lamdas=(0.1 0.5 1.0)
 for gamma in "${gammas[@]}"; do
     for lamda in "${lamdas[@]}"; do
     echo "Running LMC algorithms for gamma = $gamma and lambda = $lamda"
-        for ((i=1; i<=5; i++)); do
+        for ((i=5; i>=1; i--)); do
             echo "Running for number of mixtures n = $i"          
             python lmc_laplace.py --gamma_ula=$gamma --gamma_mala=$gamma --gamma_pula=$gamma --gamma_mla=$gamma --lamda=$lamda --alpha=5e-1 --n=$i --K=50000
             echo "LMC algorithms with gamma = $gamma and lambda = $lamda finished for $i mixtures"
